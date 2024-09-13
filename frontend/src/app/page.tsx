3fe2f98269c08c652c7cba09e863e5752917e951
Ubuntu <ubuntu@ip-172-31-6-118.ec2.internal>
2024-09-13 00:25:39 +0000

    Atv Feature flag

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,15 @@
-export default function Home() {
+import { blackFridayFlag } from './flag';
+export default async function Page() {
+  const black_friday = await blackFridayFlag();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono
-text-sm lg:flex">
-        <h1> Welcome to the ConchaYOro App - deploy v2 </h1>
-      </div>
-    </main>
+    <html>
+      <body>
+        <div>
+          <h1> Welcome to the ConchaYOro App - deploy v3a</h1>
+          { black_friday ? <button> Promo </button> : <button> Normal </button> } 
+        </div>
+      </body>
+    </html>
   )
-}
+};
